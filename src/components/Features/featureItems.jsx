0e3ac4cb056a147e7fc7381d31b6b889e64c6ceb
Tushar <tusharmohan0416@gmail.com>
2024-04-18 03:26:14 +0530

    change 3

--- src/components/Features/featureItems.jsx
+++ src/components/Features/featureItems.jsx
@@ -1,19 +1,23 @@
+import featureimg1 from "../../images/features1.jpg";
+import featureimg2 from "../../images/features2.jpg";
+import featureimg3 from "../../images/features3.jpg";
+
 const featureItems = [
     {
         id: 1,
-        image: "../../src/images/features1.jpg",
+        image: featureimg1,
         title: "Optimization",
         text: "alskdjfas"
     },
     {
         id: 2,
-        image: "../../images/features2.jpg",
+        image: featureimg2,
         title: "Productivity",
         text: "alskdjfas"
     },
     {
         id: 3,
-        image: "../../images/features3.jpg",
+        image: featureimg3,
         title: "Workflow",
         text: "alskdjfas"
     }
