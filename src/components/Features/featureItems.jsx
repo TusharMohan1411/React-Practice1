4a8139657cf1d6fd512a81efa41c266d1763773b
Tushar <tusharmohan0416@gmail.com>
2024-04-18 03:01:56 +0530

    change

--- src/components/Features/featureItems.jsx
+++ src/components/Features/featureItems.jsx
@@ -1,19 +1,19 @@
 const featureItems = [
     {
         id: 1,
-        image: "../../src/images/features1.jpg",
+        image: "../../images/features1.jpg",
         title: "Optimization",
         text: "alskdjfas"
     },
     {
         id: 2,
-        image: "../../src/images/features2.jpg",
+        image: "../../images/features2.jpg",
         title: "Productivity",
         text: "alskdjfas"
     },
     {
         id: 3,
-        image: "../../src/images/features3.jpg",
+        image: "../../images/features3.jpg",
         title: "Workflow",
         text: "alskdjfas"
     }
